72a1a292dbf3489c8684d566a891a89e8a2248ec
lMarcelo12 <silpan12@hotmail.com>
2018-10-22 18:27:04 -0500

    def

---
+++ app/src/main/java/com/example/angel/easyorder/Perfil/PerfilActivity.java
@@ -0,0 +1,15 @@
+package com.example.angel.easyorder.Perfil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.angel.easyorder.R;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+}
