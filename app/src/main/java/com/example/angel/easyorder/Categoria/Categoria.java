38b75a159948903b89183702516d662b38e645f3
lMarcelo12 <silpan12@hotmail.com>
2018-10-22 18:14:07 -0500

    Clases

--- app/src/main/java/com/example/angel/easyorder/Categoria/Categoria.java
+++ app/src/main/java/com/example/angel/easyorder/Categoria/Categoria.java
@@ -3,6 +3,8 @@ package com.example.angel.easyorder.Categoria;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.angel.easyorder.R;
+
 public class Categoria extends AppCompatActivity {
 
     @Override
