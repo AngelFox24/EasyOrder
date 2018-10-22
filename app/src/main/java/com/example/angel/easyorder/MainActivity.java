dfacce7375ab5c2ace726847cabc952f6e73c646
martin100000 <python3012@gmail.com>
2018-10-22 18:14:11 -0500

    Version Martin Codigo QR

--- app/src/main/java/com/example/angel/easyorder/MainActivity.java
+++ app/src/main/java/com/example/angel/easyorder/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.angel.easyorder;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,31 +14,34 @@ import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
-    Button btnEscanear;
-    TextView lblTextoEscaneado;
-    private ZXingScannerView mScannerView;
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView myScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnEscanear=findViewById(R.id.btnEscanear);
-        lblTextoEscaneado =findViewById(R.id.lblTextoEscaneado);
     }
-    public void Escanear(View v){
-        mScannerView=new ZXingScannerView(this);
-        setContentView(mScannerView);
-        mScannerView.setResultHandler(this);
-        mScannerView.startCamera();
+
+    public void btnEscanear(View v){
+        myScanner=new ZXingScannerView(this);
+        setContentView(myScanner);
+        myScanner.setResultHandler(this);
+        myScanner.startCamera();
+
     }
+
 
     @Override
     public void handleResult(Result result) {
-        lblTextoEscaneado.setText(result.getText());
-        Toast toast = Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_LONG);
-        toast.show();
-        mScannerView.resumeCameraPreview(this);
+        Log.v("HandleResult",result.getText());
+        AlertDialog.Builder builer=new AlertDialog.Builder(this);
+        builer.setTitle("Resultado del scaner");
+        builer.setMessage(result.getText());
+        AlertDialog alerta=builer.create();
+        alerta.show();
+        myScanner.stopCamera();
+        //myScanner.resumeCameraPreview(this);
 
     }
 }
