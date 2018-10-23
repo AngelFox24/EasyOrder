920489cf1aaaf9dfdf709274bd65eaeffdb78f8b
AngelFox24 <curilaurente@gmail.com>
2018-10-22 19:29:05 -0500

    Carrito

---
+++ app/src/main/java/com/example/angel/easyorder/shopping_cart/Shopping_Cart.java
@@ -0,0 +1,16 @@
+package com.example.angel.easyorder.shopping_cart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.angel.easyorder.R;
+
+public class Shopping_Cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping__cart);
+    }
+
+}
